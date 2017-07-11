a2aeac1d3c804012c155d935162b25a901e0b2f9
luminesty <luminesty@gmail.com>
2017-07-11 22:59:32 +0900

    add: example of topic model

--- topicModel/EM.py
+++ topicModel/EM.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 
-
+# Algorithm 2
 def initialize():
     theta = np.array()
     phi = np.array()
-    
+
+# Algorithm 1
 def EM():
     initialize()
     iter = 1
@@ -15,5 +16,26 @@ def EM():
         
     return
 
+# Algorithm 4
+def M(psi, probs):
+
+    # Optimize theta_uz
+
+    # Optimize phi_z = [phi_z1, ... , phi_zI] with gradient descent
+    
+    return psi
+    
 if __name__=="__main__":
+    data = pd.read_csv('./data/대전시.csv', sep='\t', index_col=False)
+    
+    # nUser refers to the N in the Geotopic paper (the number of users)
+    nUser = data["Member ID"].unique()
+
+    # nLoc refers to the I in the Geotopic paper (the number of locations)
+    nLoc = data["Restaurant Address"].unique()
+
+    # nTopic refers to the Z in the paper (the number of latent topics)
+    nTopic = input()
+
+    initTheta, initPhi = initialize(nUser, nTopic, nLoc)
     EM()
