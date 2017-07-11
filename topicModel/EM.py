cad3c821e7e3a22de0779e250b6d35670bf56119
luminesty <luminesty@gmail.com>
2017-07-11 20:56:54 +0900

    add: topic model

---
+++ topicModel/EM.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+
+def initialize():
+    theta = np.array()
+    phi = np.array()
+    
+def EM():
+    initialize()
+    iter = 1
+    while iter < max_iter:
+        E()
+        M()
+        
+    return
+
+if __name__=="__main__":
+    EM()
