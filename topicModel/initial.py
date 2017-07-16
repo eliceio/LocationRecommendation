34633bc2aecdd10adde4594728d434ebb17352f0
Junghyun <dpnia@Junghyuns-MacBook-Pro.local>
2017-07-16 15:12:02 +0900

    fix: fix initial.py to add "Estimation" fucntion
    But the function is not perfect and is needed to program more

--- topicModel/initial.py
+++ topicModel/initial.py
@@ -13,3 +13,25 @@ def initialize(nUser, nTopic, nLoc):
 
 
 	return [theta, phi]
+
+
+def Estimation():
+	nUser = None
+	nTopic = None
+	nLoc = None
+
+	# Initialize parameters psi = {theta, phi} (Algorithm 2)
+	theta, phi = initialize(nUser, nTopic, nLoc)
+
+	# Set the value of max_iter, beta arbitrarily
+	max_iter = 1000
+
+	for i in range(max_iter):
+		# Proceed E-step
+		# Proceed M-step
+		theta, phi = algo4(theta, phi, algo3(theta, phi))
+		
+		if converge(theta, phi) == True:
+			return
+
+	return
