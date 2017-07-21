163eb4564a99ef657cea199022efa5707b762d8c
jetBlack82 <pep_0@naver.com>
2017-07-21 16:21:42 +0900

    fix: M func

--- topicModel/topicModeling.py
+++ topicModel/topicModeling.py
@@ -6,83 +6,6 @@ import tensorflow as tf
 import pdb
 
 
-def load_data(filename):
-    df = pd.read_csv(filename, delimiter='\t', index_col=False)
-    return df
-
-def initialize(N, Z, I):
-    '''
-    Initialize theta and phi
-    theta shape: N * Z
-    phi shape: Z * I
-    '''
-    theta = np.random.rand(N, Z)
-    row_sum = theta.sum(axis=1).reshape(-1, 1)
-    theta = theta / row_sum
-    phi = np.random.rand(Z, I)
-    
-    return [theta, phi]
-
-def getDist(beta, location):
-    '''
-    input
-    beta: scalar
-    location: should be sorted list
-    
-    output
-    distance: dataframe, shape of I * I
-    '''
-    I = len(location)
-    L = np.array([[x[1], x[2]] for x in location])
-    dist = squareform(pdist(np.exp(-0.5*beta*L)))
-    loc_id = np.array([x[0] for x in location])
-    distance = pd.DataFrame(dist, columns=loc_id, index=loc_id)
-    distance[distance == 0] = 1
-    return distance
-
-def get_Xum(df):
-    df_temp = df.sort_values('Member ID')
-    df_user = df_temp[["Member ID", "Restaurant ID"]]
-
-    x_um = {}
-    for index, row in df_user.iterrows():
-        if row["Member ID"] not in x_um:
-            x_um[row["Member ID"]] = [row["Restaurant ID"]]
-        else:
-            x_um[row["Member ID"]].append(row["Restaurant ID"])
-
-    return x_um
-
-def get_P_Xum(location, df_dist, x_um, psi):
-    
-    loc_id = np.array([x[0] for x in location])
-    phi = psi[1]; phi = np.exp(phi)
-    phi = pd.DataFrame(phi, columns=loc_id)
-    pXum = {}; I = len(loc_id); Z = phi.shape[0]
-    for key in x_um.keys():
-        temp = np.full([Z, I], np.nan)
-        df_temp = pd.DataFrame(temp, columns=loc_id)
-        usr_vsted_loc = x_um[key]
-        userdist = df_dist.loc[usr_vsted_loc, usr_vsted_loc]
-        userdistSum = userdist.sum(axis=1)
-        usr_phi = phi.loc[:, usr_vsted_loc]
-        prob = usr_phi * userdistSum
-        prob = prob / prob.sum()
-
-        df_temp[usr_vsted_loc] = prob
-        pXum[key] = df_temp.fillna(0)
-        
-    return pXum
-
-
-import numpy as np
-import pandas as pd
-from scipy.optimize import minimize
-from scipy.spatial.distance import pdist, squareform
-import tensorflow as tf
-import pdb
-
-
 def load_data():
     df = pd.read_csv('deajeon.csv', sep='\t', index_col=False)
     return df
@@ -231,7 +154,6 @@ def M(x_um, p_hat, Psi, pXum):
     feed_dict = {P_hat : p_hat,
                  Theta : theta,
                  Phi : phi,
-                 #Dist : dist,
                  P : pXum}
     
     
