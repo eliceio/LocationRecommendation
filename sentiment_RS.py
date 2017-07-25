851e3e1ed7b4665a26507965a4c39216c16f7b52
luminesty <luminesty@gmail.com>
2017-07-25 19:40:21 +0900

    add: get_pref_mats func

--- sentiment_RS.py
+++ sentiment_RS.py
@@ -1,14 +1,66 @@
 import pandas as pd
 import numpy as np
 
+'''
+Auther: Sumin Lim (KAIST)
+
+This program implements the paper - Yang D. et al. "A Sentiment-Enhanced Personalized Location Recommendation System"
+
+'''
 def load_data():
+    '''
+    load data. This file uses temp data (daejeon.csv)
+    '''
     df = pd.read_csv('/data/daejeon.csv', delimiter='\t', index_col=False)
     return df
 
 def get_pref_mats(df):
+    '''
+    This function generates the check-in matrix and sentiment-preference matrix
+    
+    Input:
+    df, dataFrame. Return from load_data function
+
+    Output:
+    1. pref_checkin, check-in preference matrix
+    2. pref_sentiment, sentiment preference matrix
+    '''
+    mem_id = sorted(df['Member ID'].unique()); loc_id = sorted(df['Restaurant ID'].unique())
+    pref_checkin = pd.DataFrame(0, index=mem_id, columns=loc_id)
+    pref_sentiment = pd.DataFrame(0, index=mem_id, columns=loc_id)
+    
+    for index, row in df.iterrows():
+        # make sentiment preference matrix
+        member = row['Member ID']; restaurant = row['Restaurant ID']; rating = row['Rating']
+        pref_sentiment.loc[member, restaurant] = rating
+        checkin = pref_checkin.loc[member, restaurant]
+        if checkin == 0:
+            checkin = 1
+        elif checkin > 0:
+            checkin += 1
+
+        if checkin >= 3:
+            checkin = 3
+
+        pref_checkin.loc[member, restaurant] = checkin
+
+    pref_checkin = np.array(pref_checkin); pref_sentiment = np.array(pref_sentiment)
+    
     return pref_checkin, pref_sentiment
 
 def compute_pref_final(pref_checkin, pref_sentiment):
+    '''
+    This function calculates the final preference matrix. 
+    Equation (1) from the paper
+    
+    Input:
+    1. pref_checkin, check-in preference matrix
+    2. pref_sentiment, sentiment preference matrix
+
+    Output:
+    pref_final, final preference matrix
+    '''
+    pref_final = pref_checkin - np.sign(pref_checkin - pref_sentiment) * np.
     return pref_final
 
 def main():
