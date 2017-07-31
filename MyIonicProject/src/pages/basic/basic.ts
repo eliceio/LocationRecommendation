5bed8b3a9c8f8446be57e582119b0ae4a0e40954
Junghyun Kim <kjh4344@naver.com>
2017-07-31 21:23:28 +0900

    add: ionic project

---
+++ MyIonicProject/src/pages/basic/basic.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+
+import { Platform, ActionSheetController } from "ionic-angular";
+
+@Component({
+  selector: 'page-basic',
+  templateUrl: 'basic.html'
+})
+export class BasicPage {
+  constructor(
+    public platform: Platform,
+    public actionsheetCtrl: ActionSheetController
+  ) { }
+
+  openMenu() {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: 'Albums',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: () => {
+            console.log('Delete clicked');
+          }
+        },
+        {
+          text: 'Share',
+          icon: !this.platform.is('ios') ? 'share' : null,
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: 'Play',
+          icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
+          handler: () => {
+            console.log('Play clicked');
+          }
+        },
+        {
+          text: 'Favorite',
+          icon: !this.platform.is('ios') ? 'heart' : null,
+          handler: () => {
+            console.log('')
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+
+    });
+    actionSheet.present();
+  }
+}
