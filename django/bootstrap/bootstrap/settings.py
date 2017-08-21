095e1a99ec257df325ad3a24ea0fbeaf25f4e970
Junghyun <dpnia@Junghyuns-MacBook-Pro.local>
2017-08-22 02:03:54 +0900

    feat: not worked autoreload

--- django/bootstrap/bootstrap/settings.py
+++ django/bootstrap/bootstrap/settings.py
@@ -12,14 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
-import uwsgi
-from uwsgidecorators import timer
-from django.utils import autoreload
-
-@timer(3)
-def change_code_gracefull_reload(sig):
-    if autoreload.code_changed():
-        uwsgi.reload()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
