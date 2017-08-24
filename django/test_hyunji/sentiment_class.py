d5ece7d8e39b62f2f1c2504e5462a11c4109f50c
Ubuntu <dscourse@elice-guest-05.jt45yjj4ynve3kndnjkxlvbfse.psx.internal.cloudapp.net>
2017-08-24 08:40:23 +0000

    feat: added hyunji test

---
+++ django/test_hyunji/sentiment_class.py
@@ -0,0 +1,283 @@
+'''
+@author: Sumin Lim
+
+Class version
+'''
+
+import numpy as np
+import scipy as sp
+import pandas as pd
+from scipy.optimize import minimize
+from scipy.stats import pearsonr, logistic
+from scipy.special import expit
+
+def get_log_posterior(U, V, pref_final, sim_u, sim_v, lambda_u, lambda_v, alpha, beta, N, I, Z):
+    '''
+        Calculate the log posterior probability of U and V keeping the variance parameter fixed. 
+        In later, minimize the log posterior which is the return value of this function
+    '''
+    U = sp.resize(U, (N, Z))
+    V = sp.resize(V, (Z, I))
+    first_term = np.sum(pref_final - expit(U @ V))
+    second_term = lambda_u * np.sum(U @ U.T) + lambda_v * np.sum(V @ V.T)
+    third_term = alpha * np.sum((U - (sim_u @ U)) @ (U - (sim_u @ U)).T)
+    fourth_term = beta * np.sum((V.T - (sim_v @ V.T)) @ (V.T - (sim_v @ V.T)).T)
+    log_posterior = 0.5 * (first_term + second_term + third_term + fourth_term)
+        
+    return log_posterior
+
+def get_grad_u(U, V, pref_final, sim_u, sim_v, lambda_u, lambda_v, alpha, beta, N, I, Z):
+        
+    U = sp.resize(U, (N, Z))
+    V = sp.resize(V, (Z, I))
+        
+    grad_u_first = (logistic.pdf(U @ V) * (expit(U @ V) - pref_final)) @ V.T
+    grad_u_second = lambda_u * U + alpha * (U - sim_u @ U)
+    grad_u_third = -alpha * (sim_u @ (U - sim_u @ U))
+    grad_u = grad_u_first + grad_u_second + grad_u_third
+
+    grad_u = np.ndarray.flatten(grad_u)
+
+    return grad_u
+
+
+def get_grad_v(U, V, pref_final, sim_u, sim_v, lambda_u, lambda_v, alpha, beta, N, I, Z):
+
+    U = sp.resize(U, (N, Z))
+    V = sp.resize(V, (Z, I))
+        
+    grad_v_first = (logistic.pdf(U @ V) * (expit(U @ V)-pref_final)).T @ U
+    grad_v_second = (lambda_v * V).T + beta * (V.T - sim_v @ V.T)
+    grad_v_third = -beta * (sim_v @ (V.T - sim_v @ V.T))
+    grad_v = grad_v_first + grad_v_second + grad_v_third
+
+    grad_v = np.ndarray.flatten(grad_v)
+
+    return grad_v
+
+
+
+class SentimentRecommend():
+    '''
+    self 로 처리할 것들:
+       df, U, V, N, Z, I
+        pref_final, sim_u, sim_v,
+        lambda_v, lambda_u
+        alpha, beta
+    ''' 
+    def __init__(self, df, num_latent):
+
+        self.df = df
+        self.Z = num_latent
+
+        # # Set parameters
+        # self.U = np.zeros([self.N, self.Z])
+        # self.V = np.zeros([self.Z, self.I])
+
+    def __initialize(self):
+        # input: self.df
+        # output: self.pref_checkin, self.pref_sentiment
+        self.__get_pref_mats()
+
+        # input: self.pref_checkin, self.pref_sentiment
+        # output: self.pref_final
+        self.__compute_pref_final()
+
+        # input: self.pref_final
+        # ouput: train, test
+        self.__split_train_test()
+
+        self.N, self.I = self.pref_final.shape
+        
+        print("initialize U, V")
+        self.U = np.random.rand(self.N, self.Z)
+        self.V = np.random.rand(self.Z, self.I)
+
+        # input: self.pref_final
+        # output: self.sim_u, self.sim_v
+        print("get similarity of U, V")
+        self.__get_sim_u()
+        self.__get_sim_v()
+
+        # all self.
+        # input: pref_final, sim_u, sim_v, U, V
+        # output: lambda_u, lambda_v, alpha, beta
+        self.__get_coefficient()
+
+
+    def __setTrainModuel(self):
+        N = self.N
+        I = self.I
+        Z = self.Z
+
+    def __get_pref_mats(self):
+
+        print("making preference matrices")
+
+        mem_id = sorted(self.df['Member ID'].unique()); loc_id = sorted(self.df['Restaurant ID'].unique())
+        pref_checkin = pd.DataFrame(0, index=mem_id, columns=loc_id)
+        pref_sentiment = pd.DataFrame(0, index=mem_id, columns=loc_id)
+        
+        for index, row in self.df.iterrows():
+            # make sentiment preference matrix
+            member = row['Member ID']; restaurant = row['Restaurant ID']; rating = row['Rating']
+            pref_sentiment.loc[member, restaurant] = rating
+            checkin = pref_checkin.loc[member, restaurant]
+            if checkin == 0:
+                checkin = 1
+            elif checkin > 0:
+                checkin += 1
+
+            if checkin >= 3:
+                checkin = 3
+
+            pref_checkin.loc[member, restaurant] = checkin
+
+        self.pref_checkin = np.array(pref_checkin) 
+        self.pref_sentiment = np.array(pref_sentiment)
+        
+
+    def __compute_pref_final(self):
+        print("making final preference matrix")
+        self.pref_final = self.pref_checkin - np.sign(self.pref_checkin - self.pref_sentiment) * np.heaviside(np.abs(self.pref_checkin - self.pref_sentiment)-2, 0.5)
+    
+    def __get_sim_u(self):
+        N, _ = self.pref_final.shape
+        sim_u = []
+        for n in range(N):
+            temp = []
+            for i in range(N):
+                temp.append(pearsonr(self.pref_final[n], self.pref_final[i])[0])
+            sim_u.append(temp)
+
+        self.sim_u = np.array(sim_u)
+
+    def __get_sim_v(self):
+        location = []
+        for index, row in self.df.iterrows():
+            tmp = [row['Restaurant ID'], row['Restaurant code']]
+            if tmp not in location:
+                location.append(tmp)
+
+        sim_v = []
+        I = len(location)
+        
+        for i in range(I):
+            temp = []
+            current_code = location[i][1]
+            for j in range(I):
+                if location[j][1] == current_code:
+                    temp.append(1)
+                else:
+                    temp.append(0)
+            sim_v.append(temp)
+        self.sim_v = sim_v
+
+    def __get_coefficient(self):
+        print("get coefficient")
+        var_R = np.var(self.pref_final)
+        self.lambda_u = var_R / np.var(self.U)
+        self.lambda_v = var_R / np.var(self.V)
+        self.alpha = var_R / np.var(self.sim_u)
+        self.beta = var_R / np.var(self.sim_v)
+    
+    def __split_train_test(self):
+        print("making training set and test set")
+        test = np.zeros(self.pref_final.shape)
+        train = self.pref_final.copy()
+        N = self.pref_final.shape[0]
+        for user in range(N):
+            visited = self.pref_final[user, :].nonzero()[0]
+            if len(visited) == 1:
+                pass
+            elif len(visited) > 3:
+                test_pref = np.random.choice(visited, size=3, replace=False)
+                train[user, test_pref] = 0.
+                test[user, test_pref] = self.pref_final[user, test_pref]
+
+        # Check train and test set is independent
+        assert (np.all((train * test) == 0))
+        
+        self.train = train 
+        self.test = test
+
+
+        # input: U, V, pref_final
+    def __compute_metrics(self):
+        R_hat = self.U @ self.V
+        T = self.pref_final.shape[0] * self.pref_final.shape[1]
+        MAE = np.sum(np.abs(self.pref_final - R_hat)) / T
+        RMSE = np.sqrt(np.sum(np.square(self.pref_final - R_hat)) / T)
+
+        self.R_hat = R_hat
+        return MAE, RMSE
+
+    def print_r_hat(self):
+        print(self.R_hat)
+
+
+
+    def trainParams(self, maxiter, threshold, display=True):
+        # Initialize
+        self.__initialize()
+        niter = 0
+
+        self.__print_posterior(niter, display)
+
+        cnt = 0
+        for niter in range(1, maxiter):
+            cnt += 1
+            print("==================== In the While Loop =======================")
+            print(" %d th iteration" % cnt)
+
+            u_res = minimize(get_log_posterior,x0 = self.U, args = (self.V, self.train, self.sim_u, self.sim_v, self.lambda_u, self.lambda_v, self.alpha, self.beta, self.N, self.I, self.Z),jac = get_grad_u)
+
+            v_res = minimize(get_log_posterior,x0 = self.V, args = (self.U, self.train, self.sim_u, self.sim_v, self.lambda_u, self.lambda_v, self.alpha, self.beta, self.N, self.I, self.Z),jac = get_grad_v)
+
+            estimated_U = u_res.x.reshape(self.N, self.Z)
+            estimated_V = v_res.x.reshape(self.Z, self.I)
+
+
+            condition = np.sqrt(np.sum(np.square(self.U - estimated_U)) + np.sum(np.square(self.V - estimated_V))) < threshold
+
+            if (condition is True):
+                break
+
+            # Update parameters
+            self.U, self.V = estimated_U, estimated_V
+            
+            #self.__print_posterior(niter, display)
+
+            # Update parameters: lambda_u, lambda_v, alpha, beta
+            self.__get_coefficient()
+
+        ####### Utils to print likelihood for each iteration #######
+    
+        MAE, RMSE = self.__compute_metrics()
+        print("\n\n=========================================================")
+        print("testing")
+        print("MAE:", MAE)
+        print("RMSE:", RMSE)
+    
+
+    #def get_log_posterior(U, V, pref_final, sim_u, sim_v, lambda_u, lambda_v, alpha, beta, N, I, Z)
+
+    def __computeLikelihood(self):
+        U = sp.resize(self.U, (self.N, self.Z))
+        V = sp.resize(self.V, (self.Z, self.I))
+        first_term = np.sum(self.pref_final - expit(U @ V))
+        second_term = self.lambda_u * np.sum(U @ U.T) + self.lambda_v * np.sum(V @ V.T)
+        third_term = self.alpha * np.sum((U - (self.sim_u @ U)) @ (U - (self.sim_u @ U)).T)
+        fourth_term = self.beta * np.sum((V.T - (self.sim_v @ V.T)) @ (V.T - (self.sim_v @ V.T)).T)
+        log_posterior = 0.5 * (first_term + second_term + third_term + fourth_term)
+        
+        return log_posterior
+
+    # posterior == likelihood
+    def __print_posterior(self, niter, display):
+        if display is True:
+            L = self.__computeLikelihood()
+            if niter is 0:
+                print('Initial Log Posterior : %.3f' % L)
+            else:
+                print('Iter : %d, L value : %.3f' % (niter, L))
---
+++ django/test_hyunji/sentiment_example.py
@@ -0,0 +1,350 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+from scipy.stats import pearsonr, logistic
+from scipy.special import expit
+from scipy.optimize import minimize
+import time
+import pdb
+
+'''
+Auther: Sumin Lim (KAIST)
+
+This program implements the paper - Yang D. et al. "A Sentiment-Enhanced Personalized Location Recommendation System"
+
+'''
+def load_data():
+    '''
+    load data. This file uses temp data (daejeon.csv)
+    '''
+    df = pd.read_csv('Daejeon_dataset.csv', delimiter='\t', index_col=False)
+    return df
+
+def get_pref_mats(df):
+    '''
+    This function generates the check-in matrix and sentiment-preference matrix
+    
+    Input:
+    df, dataFrame. Return from load_data function
+
+    Output:
+    1. pref_checkin, check-in preference matrix
+    2. pref_sentiment, sentiment preference matrix
+    '''
+    mem_id = sorted(df['Member ID'].unique()); loc_id = sorted(df['Restaurant ID'].unique())
+    pref_checkin = pd.DataFrame(0, index=mem_id, columns=loc_id)
+    pref_sentiment = pd.DataFrame(0, index=mem_id, columns=loc_id)
+    
+    for index, row in df.iterrows():
+        # make sentiment preference matrix
+        member = row['Member ID']; restaurant = row['Restaurant ID']; rating = row['Rating']
+        pref_sentiment.loc[member, restaurant] = rating
+        checkin = pref_checkin.loc[member, restaurant]
+        if checkin == 0:
+            checkin = 1
+        elif checkin > 0:
+            checkin += 1
+
+        if checkin >= 3:
+            checkin = 3
+
+        pref_checkin.loc[member, restaurant] = checkin
+
+    pref_checkin = np.array(pref_checkin); pref_sentiment = np.array(pref_sentiment)
+    
+    return pref_checkin, pref_sentiment
+
+def compute_pref_final(pref_checkin, pref_sentiment):
+    '''
+    This function calculates the final preference matrix. 
+    Equation (1) from the paper
+    
+    Input:
+    1. pref_checkin, check-in preference matrix
+    2. pref_sentiment, sentiment preference matrix
+
+    Output:
+    pref_final, final preference matrix
+    '''
+    pref_final = pref_checkin - np.sign(pref_checkin - pref_sentiment) * np.heaviside(np.abs(pref_checkin - pref_sentiment)-2, 0.5)
+    return pref_final
+
+
+def split_train_test(pref_final):
+    '''
+    Split train and test set
+    '''
+
+    test = np.zeros(pref_final.shape)
+    train = pref_final.copy()
+    N = pref_final.shape[0]
+    for user in range(N):
+        visited = pref_final[user, :].nonzero()[0]
+        if len(visited) == 1:
+            pass
+        elif len(visited) > 5:
+            test_pref = np.random.choice(visited, size=1, replace=False)
+            train[user, test_pref] = 0.
+            test[user, test_pref] = pref_final[user, test_pref]
+
+    # Check train and test set is independent
+    assert (np.all((train * test) == 0))
+    
+    return train, test
+
+
+def get_UV(N, I, Z):
+    '''
+    This function initialize two matrices U and V
+
+    Input:
+    1. N: the number of users
+    2. I: the number of locations (items)
+    3. Z: the dimension of latent space 
+
+    Output:
+    randomly initialized U,V 
+    '''
+    U = np.random.rand(N, Z)
+    V = np.random.rand(Z, I)
+
+    return U, V
+
+def get_sim_u(pref_final):
+    '''
+    This function returns similarity of users
+
+    Input: pref_final, which is the return matrix of the compute_pref_final function
+    Output: similarity of users, N * N matrix
+    '''
+    N, _ = pref_final.shape
+    sim_u = []
+    for n in range(N):
+        temp = []
+        for i in range(N):
+            temp.append(pearsonr(pref_final[n], pref_final[i])[0])
+        sim_u.append(temp)
+
+    sim_u = np.array(sim_u)
+    return sim_u
+
+
+def get_sim_v(df):
+    '''
+    For two venues, the similarity score is set to 1 if both venues have the same sub-category in Foursquare
+    and set 0 if there is no overlapping sub-category
+
+    If two restaurants have the same cuisine code, similarity score is set to 1, else 0.
+    '''
+    location = []
+    for index, row in df.iterrows():
+        tmp = [row['Restaurant ID'], row['Restaurant code']]
+        if tmp not in location:
+            location.append(tmp)
+
+    sim_v = []
+    I = len(location)
+    
+    for i in range(I):
+        temp = []
+        current_code = location[i][1]
+        for j in range(I):
+            if location[j][1] == current_code:
+                temp.append(1)
+            else:
+                temp.append(0)
+        sim_v.append(temp)
+        
+    return sim_v
+
+
+def get_coefficient(pref_final, sim_u, sim_v, U, V):
+    '''
+    lambda_u = sigma^2_R / sigma^2_U
+    lambda_v = sigma^2_R / sigma^2_V
+    alpha = sigma^2_R / sigma^2_simU
+    beta = sigma^2_R / sigma^2_simV
+    '''
+    var_R = np.var(pref_final)
+    lambda_u = var_R / np.var(U)
+    lambda_v = var_R / np.var(V)
+    alpha = var_R / np.var(sim_u)
+    beta = var_R / np.var(sim_v)
+    
+    return lambda_u, lambda_v, alpha, beta
+
+
+def get_log_posterior(U, V, pref_final, sim_u, sim_v, lambda_u, lambda_v, alpha, beta, N, I, Z):
+    '''
+    Calculate the log posterior probability of U and V keeping the variance parameter fixed. 
+    In later, minimize the log posterior which is the return value of this function
+    '''
+    U = sp.resize(U, (N, Z))
+    V = sp.resize(V, (Z, I))
+    first_term = np.sum(pref_final - expit(U @ V))
+    second_term = lambda_u * np.sum(U @ U.T) + lambda_v * np.sum(V @ V.T)
+    third_term = alpha * np.sum((U - (sim_u @ U)) @ (U - (sim_u @ U)).T)
+    fourth_term = beta * np.sum((V.T - (sim_v @ V.T)) @ (V.T - (sim_v @ V.T)).T)
+    log_posterior = 0.5 * (first_term + second_term + third_term + fourth_term)
+    
+    return log_posterior
+
+
+def get_grad_u(U, V, pref_final, sim_u, sim_v, lambda_u, lambda_v, alpha, beta, N, I, Z):
+    
+    # U = sp.resize(U, (N, Z))
+    # V = sp.resize(V, (Z, I))
+    
+    grad_u_first = (logistic.pdf(U @ V) * (expit(U @ V) - pref_final)) @ V.T
+    grad_u_second = lambda_u * U + alpha * (U - sim_u @ U)
+    grad_u_third = -alpha * (sim_u @ (U - sim_u @ U))
+    grad_u = grad_u_first + grad_u_second + grad_u_third
+
+    # grad_u = np.ndarray.flatten(grad_u)
+    # pdb.set_trace()
+    
+    return grad_u
+
+
+def get_grad_v(U, V, pref_final, sim_u, sim_v, lambda_u, lambda_v, alpha, beta, N, I, Z):
+
+    # U = sp.resize(U, (N, Z))
+    # V = sp.resize(V, (Z, I))
+    
+    grad_v_first = (logistic.pdf(U @ V) * (expit(U @ V)-pref_final)).T @ U
+    grad_v_second = (lambda_v * V).T + beta * (V.T - sim_v @ V.T)
+    grad_v_third = -beta * (sim_v @ (V.T - sim_v @ V.T))
+    grad_v = grad_v_first + grad_v_second + grad_v_third
+
+    # grad_v = np.ndarray.flatten(grad_v)
+    # pdb.set_trace()
+
+    grad_v = grad_v.T
+    
+    return grad_v
+
+
+def compute_metrics(U, V, pref_final):
+    R_hat = U @ V
+    T = pref_final.shape[0] * pref_final.shape[1]
+    MAE = np.sum(np.abs(pref_final - R_hat)) / T
+    RMSE = np.sqrt(np.sum(np.square(pref_final - R_hat)) / T)
+    return MAE, RMSE
+
+def main():
+
+    print("load data")
+    df = load_data()
+
+    # input: dataFrame
+    # output: ndarray
+    print("making preference matrices")
+    pref_checkin,pref_sentiment = get_pref_mats(df)
+
+    # input: ndarray
+    # output: ndarray
+    # Eq. (1)
+    print("making final preference matrix")
+    pref_final = compute_pref_final(pref_checkin, pref_sentiment) # R
+
+    print("making training set and test set")
+    train, test = split_train_test(pref_final)
+
+    # N: # of users
+    # I: # of locations
+    N, I = pref_final.shape
+
+    # Z: user-latent space, location-latent space
+    Z = 8
+
+    # random initialization
+    print("initialize U, V")
+    U,V = get_UV(N,I,Z)
+
+    # get similarity of venues from dataset
+    print("get similarity of U, V")
+    sim_v = get_sim_v(df)
+    
+    # while until converge:
+        # get similarity of users with pearson correlation coefficient
+    sim_u = get_sim_u(U)
+
+    print("get coefficient")
+    # lambda_u, lambda_v, alpha, beta = get_coefficient(train, sim_u, sim_v, U, V)
+
+    print("get log_posterior")
+    log_posterior = get_log_posterior(U, V, train, sim_u, sim_v, 0.005, 0.005, 0.05, 0.05, N, I, Z)
+        # in particular, objective function
+        # Eq. (14)
+
+    cnt = 0
+    start_time = time.time()
+    while True:
+        cnt += 1
+        print("==================== In the While Loop =======================")
+        print(" %d th iteration" % cnt)
+        # u_res = minimize(get_log_posterior,
+        #                  x0 = U, args = (V, train, sim_u, sim_v, lambda_u, lambda_v, alpha, beta, N, I, Z),
+        #                  method='Newton-CG',
+        #                  options={'maxiter': 100, 'disp':True},
+        #                  jac = get_grad_u)
+        
+        # estimated_U = u_res.x.reshape(N, Z)
+        
+        # v_res = minimize(get_log_posterior,
+        #                  x0 = V, args = (estimated_U, train, sim_u, sim_v, lambda_u, lambda_v, alpha, beta, N, I, Z),
+        #                  method='Newton-CG',
+        #                  options={'maxiter': 100, 'disp':True},
+        #                  jac = get_grad_v)
+
+
+        # estimated_V = v_res.x.reshape(Z, I)
+
+        new_U = U - 0.1 * get_grad_u(U, V, pref_final, sim_u, sim_v, 0.005, 0.005, 0.05, 0.05, N, I, Z)
+        new_V = V - 0.1 * get_grad_v(U, V, pref_final, sim_u, sim_v, 0.005, 0.005, 0.05, 0.05, N, I, Z)
+
+        cond_u = np.sum(np.square(U - new_U)); cond_v = np.sum(np.square(V - new_V))
+        cond = np.sqrt(cond_u + cond_v)
+        condition = cond < 0.001
+
+        print("---------------------------------------------------------------")
+        print("CONDITION")
+        print("u:", cond_u, "v:", cond_v)
+        # print()
+        # print("PARAMETERS")
+        # print("alpha:", alpha, "beta:", beta, "lambda_u:", lambda_u, "lambda_v:", lambda_v)
+        print()
+        print("condition:", condition)
+
+        if condition:
+            break
+
+        U, V = new_U, new_V
+        sim_u = get_sim_u(U)
+        # lambda_u, lambda_v, alpha, beta = get_coefficient(train, sim_u, sim_v, U, V)
+        
+
+                                      
+
+    ## 끝 U, V 
+    
+    # performance evaluation
+    ## Eq. (17) & (18)
+
+    print("time %.2f seconds are running out" % time.time() - start_time)
+    MAE, RMSE = compute_metrics(U, V, pref_final)
+    print("\n\n=========================================================")
+    print("testing")
+    print("MAE:", MAE)
+    print("RMSE:", RMSE)
+
+    R_hat = U @ V
+    result = [pref_final, U, V, R_hat]
+
+    return result
+    
+
+    # Recommendation
+    
+if __name__ == "__main__":
+    main()
